699937e5f2e9fdc2d231d13bffd4b5adb379f5d7
Instincts <feminstincts@gmail.com>
2019-12-28 14:40:38 +0100

    add glow animation

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -27,24 +27,3 @@ function GoOutFullscreen(element){
 		element.msExitFullscreen();
 	}
 }
-
-// asyncrhonously fetch the html template partial from the file directory,
-// then set its contents to the html of the parent element
-function loadHTML(url, id) {
-    $.ajax({
-        type: "get",
-        url: url,
-        contentType: "text/plain; charset=utf-8",
-        async: true,
-        dataType: "text",
-        success: function (res) {
-    		$("#"+id).html(res);
-        },
-        error: function (error) {
-        	if(error.status != 200){
-	        	alert("Error 404")
-        	}
-        	console.log(error)
-        }
-    });
-}
